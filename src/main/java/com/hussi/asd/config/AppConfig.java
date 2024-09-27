e0e87baf739b503a2e330ac25d65b89be3e3f2b2
Hussain Akhtar Wahid <hussain1585@gmail.com>
2024-09-27 12:36:15 +0530

    unstable changes

---
+++ src/main/java/com/hussi/asd/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.hussi.asd.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:your.db");
+        return dataSourceBuilder.build();
+    }
+}
